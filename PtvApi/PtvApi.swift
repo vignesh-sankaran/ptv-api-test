f57b0385e665607b948db323a853cfeaec0fede2
Vignesh Sankaran <vignesh.sankaran@me.com>
2016-01-24 16:32:09 +1100

    Initial commit

---
+++ PtvApi/PtvApi.swift
@@ -0,0 +1,19 @@
+//
+//  PtvApi.swift
+//  PtvApi
+//
+//  Created by Vignesh Sankaran on 6/01/2016.
+//  Copyright © 2016 Vignesh Sankaran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+
+func healthCheck() -> Void
+{
+    var url: String = ""
+    var request : NSMutableURLRequest = NSMutableURLRequest()
+    request.URL = NSURL()
+}
