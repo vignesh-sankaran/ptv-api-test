896ab78c016c3a829253d3326852a81a4ee8eeee
Vignesh Sankaran <vignesh.sankaran@me.com>
2016-01-29 10:21:52 +1100

    Waiting for connection text now displays on the screen

--- PtvApi/ViewController.swift
+++ PtvApi/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        let loadingText = UILabel(frame: CGRectMake(100, 100, 200, 100))
+        loadingText.text = "Connecting to PTV API..."
+        loadingText.font = UIFont.systemFontOfSize(16)
+        loadingText.textColor = UIColor.blackColor()
+        
+        self.view.addSubview(loadingText)
+        
         healthCheck()
         // Do any additional setup after loading the view, typically from a nib.
     }
